3979d2c3e4ec72b0e22813115f0c6b5ef4fa28d8
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-05 10:56:25 -0300

    feat: add authentication usercase

---
+++ src/domain/usescases/Authentication.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from "../models/AccountModel";
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth(params: AuthenticationParams): Promise<AccountModel>
+}
+''
