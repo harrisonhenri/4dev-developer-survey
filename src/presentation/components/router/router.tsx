abafbb21655c53132d4c6ec77b59569dcbd4ee55
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-10 20:11:32 -0300

    feat: add react router dom

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '@/presentation/styles/global.scss'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { Login } from '@/presentation/pages'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login}/>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
