09c865e617fdf2cc77655d4c9ffbd59d9d728522
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-10 20:44:21 -0300

    feat: add library

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '@/presentation/styles/global.scss'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Login } from '@/presentation/pages'
