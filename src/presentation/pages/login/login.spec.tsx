fb8fd87cfe91c133e60a1cb91dfa9621c4b13e94
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-10 22:24:49 -0300

    refactor: create SUT factory

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login/>)
+
+  return {
+    sut
+  }
+}
 
 describe('Login', () => {
   test('should start with initial state', () => {
-    const { getByTestId, getByRole } = render(<Login/>)
+    const { sut } = makeSut()
+    const { getByTestId, getByRole } = sut
     const formStatus = getByTestId('form-status')
     const submitButton = getByRole('button') as HTMLButtonElement
     const emailStatus = getByTestId('email-status')
