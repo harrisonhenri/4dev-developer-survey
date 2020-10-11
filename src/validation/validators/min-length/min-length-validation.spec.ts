27c414a7aa00e09f1609a756d22d85461f04a2d3
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-11 19:23:54 -0300

    refactor: refactor MinLengthValidation sut

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,13 +7,13 @@ const makeSut = (minLength: number): MinLengthValidation => new MinLengthValidat
 describe('MinLengthValidation', () => {
   test('should return error if value is invalid', () => {
     const sut = makeSut(5)
-    const error = sut.validate('123')
+    const error = sut.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('should return falsy if field is valid', () => {
     const sut = makeSut(5)
-    const error = sut.validate('12345')
+    const error = sut.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })
