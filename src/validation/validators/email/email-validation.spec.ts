e14bd0ad1d65d8c9bf575a31280d047ff2a18892
HarrisonHenri <harrisonhenrisn@onyo.com>
2020-10-11 19:27:26 -0300

    refactor: change EmailValidation

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -5,15 +5,21 @@ import faker from 'faker'
 const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
 
 describe('EmailValidation', () => {
-  test('should return error if field is empty', () => {
+  test('should return error if email is empty', () => {
     const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
-  test('should return falsy if field is valid', () => {
+  test('should return falsy if email is valid', () => {
     const sut = makeSut()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+
+  test('should return falsy if email is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
